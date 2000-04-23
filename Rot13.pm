0986f71e50abe7271bd7f0db4fba85fd7dd193ab
Julian Fondren <ayrnieu@gmail.com>
2000-04-23 00:18:54 +0000

    Import of AYRNIEU/Crypt-Rot13-0.6 from CPAN.
    gitpan-cpan-distribution: Crypt-Rot13
    gitpan-cpan-version:      0.6
    gitpan-cpan-path:         AYRNIEU/Crypt-Rot13-0.6.tar.gz
    gitpan-cpan-author:       AYRNIEU
    gitpan-cpan-maturity:     released

--- Rot13.pm
+++ Rot13.pm
@@ -1,147 +1,222 @@
-## -*- Cperl -*-
+# -*- Cperl -*-
+#-----------------------------------------------------------------------
+# Copyright (C) 1999-2000 Julian Fondren
+#-- A module implementing a simple encryption
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+#-----------------------------------------------------------------------
 package Crypt::Rot13;
-$VERSION = '0.04';
 use strict;
-use Carp;
+use vars '$VERSION';
+$VERSION = 'v0.6';
 
-#------------------------------------------------------------------------------
-## Copyright (C) 1999 julian fondren
 
-## This program is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License as
-## published by the Free Software Foundation; either version 2 of
-## the License, or (at your option) any later version.
-
-## This program is distributed in the hope that it will be
-## useful, but WITHOUT ANY WARRANTY; without even the implied
-## warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-## PURPOSE.  See the GNU General Public License for more details.
-
-## You should have received a copy of the GNU General Public
-## License along with this program; if not, write to the Free
-## Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-## MA 02111-1307 USA
-#------------------------------------------------------------------------------
-
+#=======================================================================
+# Public Methods
+#----------------------------------------------------------------------- 
+#-- construct the object
 sub new {
-  my $proto = shift;
-  my $class = ref($proto) || $proto;
-  my $self  = [];
-  bless($self, $class);
+  my ($proto, @data) = @_;
+  my $class = ref ($proto) || $proto;
+  my $self  = \@data || [];
+  bless ($self, $class);
   return $self;
 }
 
+
+#----------------------------------------------------------------------- 
+#-- dereference and return the object
 sub peek {
   my $self = shift;
-  return @{$self};
+
+  return @$self;
 }
 
+
+#----------------------------------------------------------------------- 
+#-- define the object
 sub charge {
   my $self = shift;
-  return @{$self} = @_;
+
+  return @$self = @_;
 }
 
+
+#-----------------------------------------------------------------------
+#-- rotate every ASCII letter $degree times in every element of the
+#-- object.
 sub rot13 {
   my $self = shift;
-  my $degree = shift;
-  $degree = 13 unless defined $degree;
-  croak "Invalid rotation: $degree" if ($degree > 26 or $degree < 0);
-  my @rots = ( "a-z", "b-za", "c-zab", "d-za-c", "e-za-d", "f-za-e",
-	       "g-za-f", "h-za-g", "i-za-h", "j-za-i", "k-za-j", "l-za-k",
-	       "m-za-l", "n-za-m", "o-za-n", "p-za-o", "q-za-p", "r-za-q",
-	       "s-za-r", "t-za-s", "u-za-t", "v-za-u", "w-za-v", "x-za-w",
-	       "yza-x", "za-y", "a-z");
-  my @rot13;
-  foreach ($self->peek()) {
-    eval "y/a-zA-Z/@{[$rots[$degree] . uc($rots[$degree])]}/";
-    push @rot13, $_;
+  my $degree = (@_ > 0) ? shift : 13;
+
+  if (($degree % 26) == 13) {
+    # This is a rot13, which doesn't require a complex formula.
+    my @rot13;
+    foreach my $str ($self->peek ()) {
+      $str =~ tr/a-zA-Z/n-za-mN-ZA-M/;
+      push (@rot13, $str);
+    }
+    return @rot13;
   }
-  return @rot13;
+  elsif (($degree % 26) == 0) {
+    # What is anything changed zero times?  Right.
+    return $self->peek ();
+  }
+  else {
+    my ($c, @rot13);
+  line: foreach my $str (@rot13 = $self->peek ()) {
+    char: foreach my $i (0 .. (length ($str) - 1)) {
+        if (substr ($str, $i, 1) =~ /[a-zA-Z]/) {
+          $c = substr ($str, $i, 1);
+          substr ($str, $i, 1,
+                  (($c eq lc $c)
+                   ? chr ((((ord ($c) - ord ('a')) + $degree) % 26)
+                          + ord ('a'))
+                   : chr ((((ord ($c) - ord ('A')) + $degree) % 26)
+                          + ord ('A'))));
+        }
+      } # char
+    } # line
+    return @rot13;
+  }
 }
 
 
 1;
-
 __END__
 
 =head1 NAME
 
-Crypt::Rot13 - a rotational deviator
+Crypt::Rot13 v0.6 - a simple, reversible encryption
 
 =head1 SYNOPSIS
 
-  #!/usr/bin/env perl
-  $^W = 1;
-  use strict;
   use Crypt::Rot13;
 
-  my $cryptochango = new Crypt::Rot13;
-  $cryptochango->charge("Someone help us, please!");
-  print $cryptochango->rot13(5), "\n";
+  my $rot13 = new Crypt::Rot13;
+  $rot13->charge ("The quick brown fox jumped over the lazy dog.");
+  print $rot13->rot13 (), "\n";
+  print $rot13->rot13 (245.333), "\n";
+  print $rot13->peek (), "\n";
+
+  open (F, "/etc/passwd") or die "$!";
+  $rot13->charge (<F>);
+  close (F) or die "$!";
+  print $rot13->rot13 (-13);
+
+  while (<STDIN>) {
+    $rot13->charge ($_);
+    print $rot13->rot13 ();
+  }
+
+  $rot13->charge ('a' .. 'z');
+  foreach (0 .. 26) {
+    print $rot13->rot13 ($_), "\n";
+  }
 
 =head1 DESCRIPTION
 
-Rot13 provides an object into which arrays may be placed, and
-then returned to you in altered - specifically: rotated - form. The
-above code would print, "Xtrjtsj mjqu zx, uqjfxj!". If instead of
-rot13(5), rot13(11) had been used the result would have been
-"Dzxpzyp spwa fd, awpldp!", and of course just rot13() would have
-resulted in the expected, "Fbzrbar uryc hf, cyrnfr!". Valid rot13()
-arguments are 0-26, though 0 and 26 do not alter the array.
+rot13 is a simple encryption in which ASCII letters are rotated 13
+places (see below).  This module provides an array object with methods
+to encrypt its string elements by rotating ASCII letters n places down
+the alphabet.
 
-Crypt::Rot13 has the following methods.
+Think of it this way: all of the letters of the alphabet are arranged
+in a circle like the numbers of a clock.  Also like a clock, you have
+a hand pointing at one of the letters: a.  Crypt::Rot13 turns the
+hand clockwise n times through 'b', 'c', 'd', etc, and back again to
+'a', 26 turns later.
 
-=over 2
+Crypt::Rot13 turns this hand for every letter of every string it
+contains a given number of times, the default of which is 13, or
+exactly half the number of letters in the alphabet.
 
-=item new
+=head1 PUBLIC METHODS
 
-standard constructor, this creates an array object, which is most
-convienent for mass text deviation.
-(For clarification, the object *is* an array; not a hash as most are.
-This is better suited to mass rotations.)
+=over 4
 
-=item charge
+=item * Crypt::Rot13->new (@arguments)
 
-defines the array object.
+This creates a Crypt::Rot13 object, which is a blessed array
+reference.  Any arguments given to C<new> define the array, which is
+defaultly empty.
 
-=item peek
+=item * Crypt::Rot13->charge (@arguments)
 
-returns unaltered value of array object
-NOTE: rot13 as described above does not alter the object!
+Any arguments given to C<charge> define the array.  If no arguments are
+passed, the Crypt::Rot13 array will be empty.  The arguments can be
+non-strings; see the following example.
 
-=item rot13 degree
+  my $rot13 = Crypt::Rot13->new ({'foo' => 'bar'}, 111);
+  print $rot13->peek (), "\n", $rot13->rot13 (), "\n";
 
-rotates alphabetical characters of array object degree times toward the
-Unreachable Wrapping Z, or 13 times if degree is omitted.
+=item * Crypt::Rot13->peek ()
+
+This dereferences and returns the Crypt::Rot13 object.
+
+=back
+
+(In case you are wondering, the strange method names of C<peek> and
+C<charge> are derived from my original conception of Crypt::Rot13 as a
+magical device.)
+
+=over 4  
+
+=item * Crypt;:Rot13->rot13 ($degree)
+
+Rotates ASCII alphabetical characters of each element of the array
+degree times and returns the changed array without altering the
+Crypt::Rot13 object.  The degree can be negative and a fractional part
+is ignored (to be precise, C<chr> and C<%> ignore it).
+
+Degrees effectively equal to 13 are optimized to a C<tr///>.
+Degrees effectively equal to 0 are optimized into a C<peek>.  
+
+A degree I<$d> is "effectively equal" to 13 if C<$d % 26 == 13>.
+  
 
 =back
 
 =head1 LICENSE
 
-Copyright (C) 1999 julian fondren
+Copyright (C) 1999-2000 Julian Fondren
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of
-the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
 
-This program is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the GNU General Public License for more details.
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-You should have received a copy of the GNU General Public
-License along with this program; if not, write to the Free
-Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-MA 02111-1307 USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA
+
+=head1 BUGS
+
+The algorithm of C<rot13> isn't very easy to understand.
 
 =head1 AUTHOR
 
-Julian Fondren is julian@imaji.net, and can usually be found on
-#Perl, EFnet IRC.
+Julian Fondren
 
 =head1 SEE ALSO
 
-perl(1)
-
-=cut
+perl(1) rot13(1)
--- test.pl
+++ test.pl
@@ -1,12 +1,60 @@
-BEGIN { $| = 1; print "1\n"; }
-END { $loaded ? print "ok 1\n" : print "not ok 1\n"; }
-
-$^W = 1;
-
+#!/usr/bin/env perl
 use Crypt::Rot13;
-$loaded = 1;
+use strict;
+$^W = 1; # -w
+$|  = 1; # autoflush
+sub test;
 
-# I don't want to get into return values >=(
-# all of this should work, ->rot13() rotates with the degree of 13,
-# others within the acceptable range of rotations rotate as such,
-# but don't try to 'eq' a string with the return!
+sub rot_test {
+  my $rot13 = new Crypt::Rot13 ('abcdefghijklmnopqrstuvwxyz');
+  my $rot;
+
+  foreach (0 .. 26) {
+    $rot = ($rot13->rot13 ($_))[0];
+    unless (<DATA> eq "$rot\n") {
+      return 0;
+    }
+  }
+  return 1;
+}
+  
+test (1, "loading", sub { use Crypt::Rot13; 1; });
+test (2, "checking correct rotations", \&rot_test);
+
+
+sub test {
+  my ($number, $purpose, $test) = (shift, shift, shift);
+
+  print "Test $number ($purpose)...";
+  print &$test () ? "ok\n" : "not ok\n";
+}
+
+
+__DATA__
+abcdefghijklmnopqrstuvwxyz
+bcdefghijklmnopqrstuvwxyza
+cdefghijklmnopqrstuvwxyzab
+defghijklmnopqrstuvwxyzabc
+efghijklmnopqrstuvwxyzabcd
+fghijklmnopqrstuvwxyzabcde
+ghijklmnopqrstuvwxyzabcdef
+hijklmnopqrstuvwxyzabcdefg
+ijklmnopqrstuvwxyzabcdefgh
+jklmnopqrstuvwxyzabcdefghi
+klmnopqrstuvwxyzabcdefghij
+lmnopqrstuvwxyzabcdefghijk
+mnopqrstuvwxyzabcdefghijkl
+nopqrstuvwxyzabcdefghijklm
+opqrstuvwxyzabcdefghijklmn
+pqrstuvwxyzabcdefghijklmno
+qrstuvwxyzabcdefghijklmnop
+rstuvwxyzabcdefghijklmnopq
+stuvwxyzabcdefghijklmnopqr
+tuvwxyzabcdefghijklmnopqrs
+uvwxyzabcdefghijklmnopqrst
+vwxyzabcdefghijklmnopqrstu
+wxyzabcdefghijklmnopqrstuv
+xyzabcdefghijklmnopqrstuvw
+yzabcdefghijklmnopqrstuvwx
+zabcdefghijklmnopqrstuvwxy
+abcdefghijklmnopqrstuvwxyz
